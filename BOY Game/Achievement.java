8a115a1cd1778389ccdc095386e61e2c6d96717b
nrkaki <nrkaki@student.naperville203.org>
2017-05-11 10:29:55 -0500

    5/11

---
+++ BOY Game/Achievement.java
@@ -0,0 +1,51 @@
+/**
+ * Write a description of class Achievement here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+
+import java.awt.event.KeyListener;
+
+public class Achievement implements KeyListener
+{
+    private String name;
+    private String description;
+    private ArrayList<KeyEvent> combo;
+    
+    public Achievement (String call, String info, ArrayList<KeyEvent> combination)
+    {
+        name = call;
+        description = info;
+        combo = combination;
+    }
+    
+    public String getName()
+    {
+        return name;
+    }
+    
+    public String getDescription()
+    {
+        return description;
+    }
+    
+    public void keyPressed (KeyEvent e)
+    {
+        if (e.getKeyCode() == KeyEvent.combo.get(0))
+        {
+            if (e.getKeyCode() == KeyEvent.combo.get(1))
+            {
+                if (e.getKeyCode() == KeyEvent.combo.get(2))
+                {
+                    if (e.getKeyCode() == KeyEvent.combo.get(3))
+                    {
+                        //inventory.addAchievement(this);
+                    }
+                }
+            }
+        }
+    }
+    
+    
+}
